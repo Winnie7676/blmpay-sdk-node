7ea4ade4f7ed335f6133844d5665e3b158c6fa1b
BlmTec Company <mathewchaburuma20@gmail.com>
2026-09-02 23:00:05 +0300

    Update Node example for domain-bound keys

--- examples/payment.mjs
+++ examples/payment.mjs
@@ -1,3 +1,8 @@
 import BlmPay from '../src/index.js';
-const blmpay = new BlmPay({apiKey: process.env.BLMPAY_API_KEY});
+
+const blmpay = new BlmPay({
+  apiKey: process.env.BLMPAY_API_KEY,
+  integrationOrigin: process.env.BLMPAY_INTEGRATION_ORIGIN || null,
+});
+
 console.log(await blmpay.getBalance());
